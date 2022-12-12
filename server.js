dcdf42f01d979bbab725b1fba78ed0908c1aca40
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2022-12-12 23:12:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,34 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const process = require('process');
-
-
+//Database Connection
+const connectDb = require('./connectDB');
+//Schema and Model
+const User = require('./model');
+// to use environment variables
+require('dotenv').config();
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors())
-
 mongoose.set('strictQuery', true); 
 
-mongoose.connect("mongodb+srv://Shubhendu:titan123@cluster0.hdhh4jd.mongodb.net/LoginData",
-() => {
-    console.log("DB connected")
-})
 
-
-
-//Schema and Model
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-})
-const User = new mongoose.model("UserData", userSchema)
+//Connecting with database
+connectDb();
 
 
 //Routes
-
 app.get("/", (req, res)=>{
     res.send("Ready to work.");
 })
@@ -75,21 +66,9 @@ app.post("/signup", (req, res)=> {
 
 // for deployment
 const port = process.env.PORT || 4000;
-
-// static files
-// app.use(express.static(path.join(__dirname, './client/build')));
-// app.get("*", (req,res)=>{
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// })
-
-
-// if(process.env.NODE_ENV == "production"){
-//     app.use(express.static("client/build"));
-// }
-
 app.listen(port, (err) => {
     if(err){
-        console.log(err);;
+        console.log(err);
     }
     console.log(`Server started at port ${port}`);
 })
