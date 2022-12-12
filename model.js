18a888fcd10a6aedfb437ff55ce14b896fb7596e
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2022-12-12 23:11:45 +0530

    Add files via upload

---
+++ model.js
@@ -0,0 +1,10 @@
+//Schema and Model
+
+const mongoose= require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+})
+module.exports = mongoose.model("UserData", userSchema);
